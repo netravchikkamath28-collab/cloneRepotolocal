c2eea04b3fa51a5db0aaf76363e951f87e2a91c2
netravchikkamath28-collab <netravchikkamath28@gmail.com>
2026-09-02 02:33:10 -0700

    Update collegeinfo.py

--- collegeinfo.py
+++ collegeinfo.py
@@ -2,10 +2,9 @@ college=input("enter your college name")
 city=input("enter your city name")
 contact= input("enter your college contact number")
 location= input("enter the location of the college")
-
+usn = input("enter usn")
 print("college basic details",collage ""+city)
 print("college contact is",contact)
 print("loction of the college",location)
 
-print ("enter the usn",usn)
-print ("enter the studnet name")
+print ("USN",usn)
